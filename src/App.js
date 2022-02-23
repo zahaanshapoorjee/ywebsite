468c2e2b0e84148e3521cbd3a7b5aec1dbd2f1cf
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2022-02-23 20:49:31 +0530

    ;(

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='poop1'>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+
+      </div>
+      <div className='disprow'>
+      <img  src={require('./Assets/zahaanleft.png')} alt='Failed to Load' className='zahaanuleft' ></img>
+      <img  src={require('./Assets/center.png')} alt='Failed to Load' className='center' ></img>
+      <img  src={require('./Assets/zahaanright.png')} alt='Failed to Load' className='zahaanuright' ></img>
+      </div>
+      <div className='yashu1'>
+        Yashu Pie Shapoorjee
+      </div>
+      <div className='poop2'>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+      <img  src={require('./Assets/poopemoji.png')} alt='Failed to Load' className='poop' ></img>
+
+      </div>
+      <div className='yashu2'>
+        Poopy Enthusiast
+      </div>
+    </div>
+  );
+}
+
+export default App;
